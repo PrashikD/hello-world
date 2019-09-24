29304426d759669caec4b8d977b6783800c7e45e
Prashik Deshpande <43670200+PrashikD@users.noreply.github.com>
2019-09-24 22:41:19 +0530

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,5 @@
+#include <stdio.h>     //standard input-output , this header file gets the interfaces for inputting and outputting data
+int main()             //  'main()' is the main function in any c program
+   printf(" Hello, World!! ");  // printf(), f for formatted, displays the string inside quotes
+   return 1;           //  int for the function should return an interger value, thus return 1, implies program went successfully 
+}
